a5978df596931edcc61be8a69a5f74b2f5a8a6db
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2025-12-23 00:59:31 +0530

    updated manytomany

--- ManytoMany/src/main/java/Practice/ManytoMany/App.java
+++ ManytoMany/src/main/java/Practice/ManytoMany/App.java
@@ -35,6 +35,46 @@ public class App
         techL4.add(trList2);
         techL4.add(trList3);
         
+        Tech t1 = new Tech(101, "Java", techL1);
+        Tech t2 = new Tech(102, "SQL", techL2);
+        Tech t3 = new Tech(103, "Python", techL3);
+        Tech t4 = new Tech(104, "React", techL4);
+        
+        List<Tech> tech1 = new ArrayList<>();
+        tech1.add(t1);
+        tech1.add(t3);
+        
+        List<Tech> tech2 = new ArrayList<>();
+        tech2.add(t1);
+        tech2.add(t3);
+        tech2.add(t4);
+        
+        List<Tech> tech3 = new ArrayList<>();
+        tech3.add(t2);
+        tech3.add(t4);
+        
+        trList1.setTrainerId(1);
+        trList1.setTrainerName("Ayush");
+        trList1.setTechList(tech1);
+        
+        trList2.setTrainerId(2);
+        trList2.setTrainerName("Deep");
+        trList2.setTechList(tech2);
+        
+        trList3.setTrainerId(3);
+        trList3.setTrainerName("Ankit");
+        trList3.setTechList(tech3);
+        
+        sess.persist(t1);
+        sess.persist(t2);
+        sess.persist(t3);
+        sess.persist(t4);
+        
+        sess.persist(trList1);
+        sess.persist(trList2);
+        sess.persist(trList3);
+        
+        tx.commit();
         
         
     }
