153cbb6eb9b96469636d950369984fd5000d66b7
Chandru-trigeminal <chandramohan@trigeminal-ai.com>
2025-12-25 14:21:14 +0530

    updated servlet with full stack

--- com.auth.fas/src/main/java/Servlet/DataHandler.java
+++ com.auth.fas/src/main/java/Servlet/DataHandler.java
@@ -6,19 +6,38 @@ import java.sql.SQLException;
 public class DataHandler {
 
 	public static void insertData(String uname, String password) throws SQLException {
-		String url = "jdbc:mysql://localhost:3306/servlet";
+		String url = "jdbc:mysql://locallhost:3306/servlet";
 		String username = "root";
-		String pass = "root1234";
+		String pass1 = "root1234";
 		String sql = "insert into customer values(?, ?)";
 		
-		Connection con = DriverManager.getConnection(url, username, pass);
+		Connection con = DriverManager.getConnection(url, username, pass1);
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setString(1, uname);
 		ps.setString(2, password);
 		
 		int res = ps.executeUpdate();
-		System.out.println(res + " row is added");
+		System.out.println(res + " row is added");		
+	}
+
+	public static String fetchData(String uname) throws SQLException, ClassNotFoundException {
+		String url = "jdbc:mysql://locallhost:3306/servlet";
+		String username = "root";
+		String password = "root1234";
+		String sql = "select password from customer where uname = ?";
 		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		Connection con = DriverManager.getConnection(url, username, password);
+		
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setString(1, uname);
+		
+		ResultSet rs = ps.executeQuery();
+		rs.next();
+		String res = rs.getString(1);
+		
+		return res;
 	}
 
 }
--- com.auth.fas/src/main/java/Servlet/LoginServlet.java
+++ com.auth.fas/src/main/java/Servlet/LoginServlet.java
@@ -1,32 +1,34 @@
 package Servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.SQLException;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.*;
 
-@WebServlet("/register")
-public class Register extends HttpServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
 		String uname = req.getParameter("name");
 		String password = req.getParameter("password");
 		
-		System.out.println(uname + " " + password);
-		
+		String dbPass = null;
 		try {
-			DataHandler.insertData(uname, password);
-			RequestDispatcher rd = req.getRequestDispatcher("/login.html");
-			rd.forward(req, resp);
-		} catch (SQLException e) {
+			dbPass = DataHandler.fetchData(uname);
+		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		PrintWriter pw = resp.getWriter();
+		if (password.equals(dbPass)) {
+			pw.println("Login success!");
+		} else {
+			pw.println("Login fail!");
+		}
 	}
 
 }
